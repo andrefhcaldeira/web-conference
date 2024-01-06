ae15daa391824ae3d4f28edd330a2ffc820352e2
andrefhcaldeira <andrefhcaldeira@proton.me>
2024-01-06 17:45:42 +0000

    Added nav menu and corresponding css

--- inc/top.inc.php
+++ inc/top.inc.php
@@ -6,14 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/novatasks.css">
-    <title>Document</title>
+    <title>Web Conference</title>
 </head>
 
 <body>
+<nav>
+    <a href="#">
+        <img class="logo" src="#">
+    </a>
+    <a href="#">Home</a>
+    <a href="#">Local</a>
+    <a href="#">Informations</a>
+    <a href="#">Artigos</a>
+    <a href="#">Tracks</a>
+    <div class="search-container">
+        <input type="text" placeholder="Search...">
+        <button type="submit">Search</button>
+    </div>
+    <a href="#">Login</a>
+</nav>
+<div class="hero">
 
-<header>
+</div>
+
+<!-- <header>
     <nav class="clearfix">
         <div class="left"></div>
         <div class="right"><? include "inc/check-user.inc.php"; ?></div>
     </nav>
-</header>
+</header> -->
--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include "inc/top.inc.php";
 
 ?>
 
-
 <h1>welcome to NovaTasks</h1>
 
 Please register or login.
