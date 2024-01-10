7dce1f0d8d9c4cb41b90e66b8f18556877fd2621
andrefhcaldeira <andrefhcaldeira@proton.me>
2024-01-10 13:04:20 +0000

    Added tracks example to be refined.

--- index.php
+++ index.php
@@ -14,5 +14,19 @@ include "inc/top.inc.php";
         <p class="description">Daniel M. Keem, conhecido online como Keemstar, é um Youtuber, podcaster, e Streamer. Ele é mais conhecido por ser o apresentador do programa de notícias de cultura popular, DramaAlert.</p>
     </div>
 </div>
+<h2>Tracks</h2>
+<div class="tracks">
+    <span class="track-number">1</span>
+    <div class="track-container">
+        <div class="track-left">
+            <span class="track-title">Sustainable Futures:</span>
+            <span class="track-description">Integrating Science and Technology for Environmental Harmony</span>
+        </div>
+        <div class="track-right">
+            <span class="track-date">12/02/2024</span>
+            <span class="track-room">A</span>
+        </div>
+    </div>
+</div>
 
 Please register or login.
