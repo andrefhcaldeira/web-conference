d1a5b67a941626ec5fea6888fe73c10f4ab3be3f
andrefhcaldeira <andrefhcaldeira@proton.me>
2024-01-10 14:10:48 +0000

    Added image and location page php file.

---
+++ location.php
@@ -0,0 +1,17 @@
+<?php
+
+include "inc/top.inc.php";
+
+
+?>
+
+<h1>Location</h1>
+
+<img src="./images/pexels-photo-2606383.webp" width="600px" height="auto">
+
+<?php
+
+include "inc/bot.inc.php";
+
+
+?>
